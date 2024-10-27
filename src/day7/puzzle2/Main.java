432faa1bc50bb9472e8fcbd6b214f65be8341332
Dillon-Tarr <dill.tarr@gmail.com>
2024-10-26 20:52:52 -0400

    Solved Day 7 Puzzle 2.

---
+++ src/day7/puzzle2/Main.java
@@ -0,0 +1,50 @@
+package day7.puzzle2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Main {
+    static private final String INPUT_FILE_PATH = "src/day7/input.txt";
+    static private final File INPUT_FILE = new File(INPUT_FILE_PATH);
+    static private final ArrayList<Hand> hands = new ArrayList<>();
+
+    public static void main(String[] args) {
+        getInputData();
+        sortHands();
+        printHands();
+        sumWinnings();
+    }
+
+    private static void getInputData() {
+        try (BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE))) {
+            String currentLine = br.readLine();
+            while (currentLine != null) {
+                hands.add(new Hand(currentLine.split(" ")));
+                currentLine = br.readLine();
+            }
+        } catch (IOException e) {throw new RuntimeException(e);}
+    }
+
+    private static void sortHands() {
+        hands.sort(Comparator.comparing(Hand::getScore));
+    }
+
+    private static void printHands() {
+        System.out.println("\nAll hands:");
+        for (Hand hand : hands) {
+            System.out.println(hand);
+        }
+    }
+
+    private static void sumWinnings() {
+        int winnings = 0;
+        for (int i = 0; i < hands.size(); i++)
+            winnings += hands.get(i).getBid() * (i + 1);
+        System.out.println("\nTOTAL WINNINGS:\n\n"+winnings);
+    }
+
+}
