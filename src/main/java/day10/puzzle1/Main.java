e0e6c102d50b7fb463052061581897378520e1f4
Dillon-Tarr <dill.tarr@gmail.com>
2024-10-29 16:47:14 -0400

    Solved Day 10 Puzzle 1.

---
+++ src/main/java/day10/puzzle1/Main.java
@@ -0,0 +1,99 @@
+package day10.puzzle1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+    static private final String INPUT_FILE_PATH = "input-files/day10input.txt";
+    static private final File INPUT_FILE = new File(INPUT_FILE_PATH);
+    static private int gridWidth;
+    static private int gridHeight;
+    static private char[][] grid;
+    static private int startY; // Coordinates referenced in y-x order since values are always obtained in line-character order.
+    static private int startX;
+    static private int loopLength;
+
+    public static void main(String[] args) {
+        getInputDataAndStartCoordinates();
+        evaluateLoopLength();
+        printStepsNeededToGetToFurthestPointInLoop();
+    }
+
+    private static void printStepsNeededToGetToFurthestPointInLoop() {
+        System.out.println("\nSteps needed to get to the furthest point in the loop:\n\n"+(loopLength/2));
+    }
+
+    private static void evaluateLoopLength() {
+        int stepsTaken = 0;
+        char currentCharacter = 'S';
+        int currentY = startY;
+        int currentX = startX;
+        int previousY = startY;
+        int previousX = startX;
+        do {
+            while (true) {
+                stepsTaken++;
+                if (currentY != 0 && previousY != currentY-1 && (currentCharacter == '|' || currentCharacter == 'L' || currentCharacter == 'J') && // Go up
+                        (grid[currentY -1][currentX] == '7' || grid[currentY-1][currentX] == '|' || grid[currentY-1][currentX] == 'F' || grid[currentY-1][currentX] == 'S')) {
+                    previousX = currentX;
+                    previousY = currentY;
+                    currentY--;
+                    currentCharacter = grid[currentY][currentX];
+                    break;
+                }
+                if (currentX != gridWidth-1 && previousX != currentX+1 && (currentCharacter == '-' || currentCharacter == 'L' || currentCharacter == 'F') && // Go right
+                        (grid[currentY][currentX+1] == 'J' || grid[currentY][currentX +1] == '-' || grid[currentY][currentX+1] == '7' || grid[currentY][currentX+1] == 'S')) {
+                    previousX = currentX;
+                    previousY = currentY;
+                    currentX++;
+                    currentCharacter = grid[currentY][currentX];
+                    break;
+                }
+                if (currentY != gridHeight-1 && previousY != currentY+1 && (currentCharacter == '|' || currentCharacter == 'F' || currentCharacter == '7') && // Go down
+                        (grid[currentY+1][currentX] == 'L' || grid[currentY+1][currentX] == '|' || grid[currentY+1][currentX] == 'J' || grid[currentY+1][currentX] == 'S')) {
+                    previousX = currentX;
+                    previousY = currentY;
+                    currentY++;
+                    currentCharacter = grid[currentY][currentX];
+                    break;
+                } // Else go left:
+                    previousX = currentX;
+                    previousY = currentY;
+                    currentX--;
+                    currentCharacter = grid[currentY][currentX];
+                    break;
+            }
+        } while (currentY != startY || currentX != startX);
+        loopLength = stepsTaken;
+    }
+
+    private static void getInputDataAndStartCoordinates() {
+        try (BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE))) {
+            String currentLine = br.readLine();
+            ArrayList<char[]> inputLines = new ArrayList<>();
+            while (currentLine != null) {
+                inputLines.add(currentLine.toCharArray());
+                currentLine = br.readLine();
+            }
+            gridWidth = inputLines.get(0).length;
+            gridHeight = inputLines.size();
+            grid = new char[gridHeight][];
+            for (int y = 0; y < gridHeight; y++) {
+                grid[y] = new char[gridWidth];
+                    for (int x = 0; x < gridWidth; x++) {
+                        char value = inputLines.get(y)[x];
+                        grid[y][x] = value;
+                        if (value == 'S') {
+                            startY = y;
+                            startX = x;
+                        }
+                    }
+                }
+            } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
