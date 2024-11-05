d68b4a608af7ade431fc204ec44439cdb6dd7c1d
Dillon-Tarr <dill.tarr@gmail.com>
2024-11-05 16:15:20 -0500

    Made a much better solution to Day 12 Puzzle 1.

---
+++ src/main/java/day12/puzzle1/BetterSpringRow.java
@@ -0,0 +1,58 @@
+package day12.puzzle1;
+
+import java.util.Arrays;
+
+public class BetterSpringRow {
+    private static int sumOfAllRowsArrangementCounts = 0;
+
+    public static int getSumOfAllRowsArrangementCounts() {return sumOfAllRowsArrangementCounts;}
+
+    BetterSpringRow(String dataLine) {
+        int spaceIndex = dataLine.indexOf(" ");
+        String[] groupCountStrings = dataLine.substring(spaceIndex+1).split(",");
+
+        String row = dataLine.substring(0, spaceIndex);
+        int[] groupCounts = new int[groupCountStrings.length];
+        for (int i = 0; i < groupCountStrings.length; i++) groupCounts[i] = Integer.parseInt(groupCountStrings[i]);
+
+        int minimumRemainingLength = groupCounts.length-1;
+        for (int groupCount : groupCounts) minimumRemainingLength += groupCount;
+        sumOfAllRowsArrangementCounts += countArrangements(row, groupCounts, minimumRemainingLength);
+    }
+
+    private int countArrangements(String remainingRow, int[] remainingGroupCounts, int minimumRemainingLength) {
+        if (remainingRow.isEmpty()) { // Out of spring data points, so should also be out of group counts.
+            if (remainingGroupCounts.length == 0) return 1;
+            else return 0;}
+        if (remainingGroupCounts.length == 0) {
+            if (remainingRow.contains("#")) return 0;
+            else return 1;}
+
+        int count = 0;
+
+        if ((remainingRow.length() > minimumRemainingLength) && (remainingRow.charAt(0) == '.' || remainingRow.charAt(0) == '?'))
+                count += countArrangements(discardBeginningOfString(remainingRow, 1), remainingGroupCounts, minimumRemainingLength);
+        if (remainingRow.charAt(0) == '#' || remainingRow.charAt(0) == '?') {
+            String groupString = remainingRow.substring(0, remainingGroupCounts[0]);
+            boolean noBrokenSpringRightAfterGroup = remainingRow.length() == remainingGroupCounts[0] || remainingRow.charAt(remainingGroupCounts[0]) != '#';
+
+            if (remainingRow.length() >= minimumRemainingLength && !(groupString.contains(".")) && noBrokenSpringRightAfterGroup)
+                count += countArrangements(discardBeginningOfString(remainingRow, remainingGroupCounts[0]+1),
+                        discardFirstElement(remainingGroupCounts), minimumRemainingLength-remainingGroupCounts[0]-1);
+        }
+        return count;
+    }
+
+    private String discardBeginningOfString(String string, int indexBeforeWhichToDiscard) {
+        if (indexBeforeWhichToDiscard >= string.length()) return "";
+        else return string.substring(indexBeforeWhichToDiscard);
+    }
+
+    private int[] discardFirstElement(int[] array) {
+        if (array.length <= 1) return new int[0];
+        else {
+            return Arrays.copyOfRange(array, 1, array.length);
+        }
+    }
+
+}
