a4a1fb0660c48db25b55db2d83fa10f9897d2cb8
Dillon-Tarr <dill.tarr@gmail.com>
2024-12-06 11:52:49 -0500

    Solve Day 19 Puzzle 1

---
+++ src/main/java/day19/puzzle1/Main.java
@@ -0,0 +1,64 @@
+package day19.puzzle1;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+    static private final File INPUT_FILE = new File("input-files/day19input.txt");
+    static private final HashMap<String, Workflow> workflows = new HashMap<>();
+    static private final ArrayList<Part> parts = new ArrayList<>();
+    static private final ArrayList<Part> acceptedParts = new ArrayList<>();
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
+        processInputData();
+        processParts();
+        getSumOfAcceptedPartsRatings();
+
+        System.out.println("\nExecution time in seconds: " + ((double) (System.nanoTime() - startTime) / 1000000000));
+    }
+
+    private static void processInputData() {
+        try (BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE))) {
+            boolean workflowMode = true;
+            String cl = br.readLine();
+            while (cl != null) {
+                if (cl.isEmpty()) {
+                    workflowMode = false;
+                } else if (workflowMode) {
+                    int firstBracketIndex = cl.indexOf("{");
+                    workflows.put(cl.substring(0, firstBracketIndex), new Workflow(cl.substring(firstBracketIndex+1, cl.length()-1)));
+                } else {
+                    String[] ratingStrings = cl.substring(1, cl.length()-1).split(",");
+                    int[] ratings = new int[ratingStrings.length];
+                    for (int i = 0; i < ratingStrings.length; i++) ratings[i] = Integer.parseInt(ratingStrings[i].substring(2));
+                    parts.add(new Part(ratings[0], ratings[1], ratings[2], ratings[3]));
+                }
+                cl = br.readLine();
+            }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    private static void processParts() {
+        for (Part part : parts) {
+            Workflow currentWorkflow = workflows.get("in");
+            String resultOrNextWorkflow = currentWorkflow.processPart(part);
+            while (!resultOrNextWorkflow.equals("A") && !resultOrNextWorkflow.equals("R")) {
+                currentWorkflow = workflows.get(resultOrNextWorkflow);
+                resultOrNextWorkflow = currentWorkflow.processPart(part);
+            }
+            if (resultOrNextWorkflow.equals("A")) acceptedParts.add(part);
+        }
+    }
+
+    private static void getSumOfAcceptedPartsRatings() {
+        int sum = 0;
+        for (Part part : acceptedParts) sum += part.getRatingSum();
+        System.out.println("\n\nSum of the ratings of all accepted parts: "+ sum);
+    }
+
+}
---
+++ src/main/java/day19/puzzle1/Workflow.java
@@ -0,0 +1,45 @@
+package day19.puzzle1;
+
+public class Workflow {
+    private final Rule[] rules;
+    private final String defaultResult;
+
+    Workflow (String inputString) {
+        String[] ruleStrings = inputString.split(",");
+        rules = new Rule[ruleStrings.length-1];
+        for (int i = 0; i < rules.length; i++) rules[i] = new Rule(ruleStrings[i]);
+        defaultResult = ruleStrings[ruleStrings.length-1];
+    }
+
+    String processPart(Part part) {
+        for (Rule rule : rules) if (rule.matchCheck(part)) return rule.nextWorkflow;
+        return defaultResult;
+    }
+
+    private class Rule {
+        private final char category;
+        private final boolean comparisonOperatorIsLessThan;
+        private final int comparisonValue;
+        private final String nextWorkflow;
+
+        Rule (String ruleString) {
+            int colonIndex = ruleString.indexOf(":");
+            category = ruleString.charAt(0);
+            comparisonOperatorIsLessThan = ruleString.charAt(1) == '<';
+            comparisonValue = Integer.parseInt(ruleString.substring(2, colonIndex));
+            nextWorkflow = ruleString.substring(colonIndex+1);
+        }
+
+        public boolean matchCheck(Part part) {
+            return switch (category) {
+                case 'x' -> comparisonOperatorIsLessThan ?  part.x < comparisonValue : part.x > comparisonValue;
+                case 'm' -> comparisonOperatorIsLessThan ?  part.m < comparisonValue : part.m > comparisonValue;
+                case 'a' -> comparisonOperatorIsLessThan ?  part.a < comparisonValue : part.a > comparisonValue;
+                case 's' -> comparisonOperatorIsLessThan ?  part.s < comparisonValue : part.s > comparisonValue;
+                default -> throw new RuntimeException("Invalid category: '"+category+"'");
+            };
+        }
+
+    }
+
+}
