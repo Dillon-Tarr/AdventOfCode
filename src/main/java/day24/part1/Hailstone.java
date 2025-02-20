c19e26232702a86876e267e5fdbc85893511f085
Dillon-Tarr <dill.tarr@gmail.com>
2025-02-20 14:52:49 -0500

    Solve day 24 part 1

---
+++ src/main/java/day24/part1/Hailstone.java
@@ -0,0 +1,23 @@
+package day24.part1;
+
+public class Hailstone {
+    long xPosition, yPosition, xVelocity, yVelocity;
+    boolean hasPositiveXVelocity, hasPositiveYVelocity;
+    float slope, yIntercept;
+
+    Hailstone(long xPos, long yPos, long xVel, long yVel) {
+        xPosition = xPos;
+        yPosition = yPos;
+        xVelocity = xVel;
+        hasPositiveXVelocity = xVel > 0;
+        yVelocity = yVel;
+        hasPositiveYVelocity = yVel > 0;
+        slope = (float) yVel/xVel;
+        yIntercept = yPos-(xPos*slope);
+    }
+
+    @Override
+    public String toString() {
+        return "x, y positions: "+xPosition+", "+yPosition+"\nx, y velocities: "+xVelocity+", "+yVelocity+"\nslope: "+slope+"\ny-intercept: "+yIntercept;
+    }
+}
---
+++ src/main/java/day24/part1/Main.java
@@ -0,0 +1,55 @@
+package day24.part1;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+    static private final File INPUT_FILE = new File("input-files/day24input.txt");
+    static private final ArrayList<Hailstone> hailstones = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
+        processInputData();
+        checkForCollisions();
+
+        System.out.println("\nExecution time in seconds: " + ((double) (System.nanoTime() - startTime) / 1000000000));
+    }
+
+    private static void processInputData() {
+        try (BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE))) {
+            String line = br.readLine();
+            String[] splitLine;
+            long[] longs = new long[6];
+            while (line != null) {
+                splitLine = line.split("[,@]");
+                for (int i = 0; i < splitLine.length; i++) longs[i] = Long.parseLong(splitLine[i].trim());
+                hailstones.add(new Hailstone(longs[0], longs[1], longs[3], longs[4]));
+                line = br.readLine();
+            }
+        } catch (IOException e) {throw new RuntimeException(e);}
+    }
+
+    private static void checkForCollisions() {
+        int count = 0;
+        long xAndYAreaLow = 200000000000000L, xAndYAreaHigh = 400000000000000L;
+        Hailstone a, b;
+        float intersectionX, intersectionY;
+        for (int i = 0; i < hailstones.size() - 1; i++)
+            for (int j = i + 1; j < hailstones.size(); j++) {
+                a = hailstones.get(i);
+                b = hailstones.get(j);
+                if (a.slope == b.slope) continue;
+                intersectionX = (b.yIntercept - a.yIntercept) / (a.slope - b.slope);
+                if (intersectionX < xAndYAreaLow || intersectionX > xAndYAreaHigh) continue;
+                intersectionY = a.yIntercept + (a.slope * intersectionX);
+                if (intersectionY >= xAndYAreaLow && intersectionY <= xAndYAreaHigh
+                        && a.hasPositiveXVelocity == intersectionX > a.xPosition && a.hasPositiveYVelocity == intersectionY > a.yPosition
+                        && b.hasPositiveXVelocity == intersectionX > b.xPosition && b.hasPositiveYVelocity == intersectionY > b.yPosition)
+                    count++;
+            }
+        System.out.println("\nCollision count: " + count);
+    }
+
+}
