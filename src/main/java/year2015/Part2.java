7b4173341fb4df9383c20368f759135b390bb0c7
Dillon-Tarr <dill.tarr@gmail.com>
2025-06-26 10:10:52 -0400

    Complete year 2015 day 1 part 2

---
+++ src/main/java/year2015/Part2.java
@@ -0,0 +1,50 @@
+package year2015;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Part2 {
+    static private final int DAY = 1;
+    static private final File INPUT_FILE = new File("input-files/2015/"+DAY+".txt");
+
+    static private String inputString;
+    static private int floorChangeCount = 0;
+    static private int currentFloor = 0;
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
+        getInputData();
+        traverse();
+
+        System.out.println("Floor change count upon reaching basement (floor -1): "+floorChangeCount);
+
+        System.out.println("\nExecution time in seconds: "+((double) (System.nanoTime()-startTime)/1000000000));
+    }
+
+    private static void getInputData() {
+        try (BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE))) {
+            StringBuilder sb = new StringBuilder();
+            String currentLine = br.readLine();
+            while(!(currentLine == null)) {
+                sb.append(currentLine);
+                currentLine = br.readLine();
+            }
+            inputString = sb.toString();
+        } catch (IOException e) {throw new RuntimeException(e);}
+    }
+
+    private static void traverse(){
+        for (int i = 0; i < inputString.length(); i++) {
+            floorChangeCount++;
+            switch (inputString.charAt(i)) {
+                case '(' -> currentFloor++;
+                case ')' -> currentFloor--;
+            }
+            if (currentFloor == -1) break;
+        }
+    }
+
+}
