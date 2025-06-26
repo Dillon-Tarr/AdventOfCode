8b9a68bf63b857668ac5115ea86fe125025219af
Dillon-Tarr <dill.tarr@gmail.com>
2025-06-26 14:42:43 -0400

    Solve year 2015 day 2 part 2

---
+++ src/main/java/year2015/day2/part2/Box.java
@@ -0,0 +1,37 @@
+package year2015.day2.part2;
+
+public class Box {
+    private final int[] dimensions = new int[3];
+    int ribbonNeeded;
+
+    Box(String inputString) {
+        parseDimensions(inputString);
+        calculateRibbonNeeded();
+        System.out.println(inputString+", needed: "+ribbonNeeded);
+    }
+
+    private void parseDimensions(String inputString) {
+        String[] dimensionStrings = inputString.split("x");
+        for (int i = 0; i < 3; i++) {
+            dimensions[i] = Integer.parseInt(dimensionStrings[i]);
+        }
+    }
+
+    private void calculateRibbonNeeded() {
+        int largestDimensionSize = Integer.MIN_VALUE;
+        int largestDimensionIndex = Integer.MIN_VALUE;
+        for (int i = 0; i < 3; i++) {
+            if (dimensions[i] > largestDimensionSize) {
+                largestDimensionSize = dimensions[i];
+                largestDimensionIndex = i;
+            }
+        }
+        int shortestPerimeter = 0;
+        for (int i = 0; i < 3; i++) {
+            if (i != largestDimensionIndex) shortestPerimeter += 2*dimensions[i];
+        }
+
+        ribbonNeeded = (dimensions[0]*dimensions[1]*dimensions[2])+shortestPerimeter;
+    }
+
+}
