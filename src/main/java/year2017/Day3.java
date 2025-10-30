bb09da9cf9fa1da5bf4d55b03b06366708323b9b
Dillon-Tarr <dill.tarr@gmail.com>
2025-10-30 19:38:29 -0400

    Solve year 2017 day 3 part 2

--- src/main/java/year2017/Day3.java
+++ src/main/java/year2017/Day3.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 import static shared.CardinalDirection.*;
 
@@ -19,6 +20,7 @@ class Day3 {
 
         getInputData();
         findGoalDistance(); // part 1
+        findFirstValueAboveGoalNumber(); // part 2
 
         System.out.println("\nExecution time in seconds: "+((double) (System.nanoTime()-startTime)/1000000000));
     }
@@ -56,6 +58,50 @@ class Day3 {
         }
         int distance = Math.abs(x)+Math.abs(y);
         System.out.println("\nManhattan distance from square 1 to square "+goal+" (part 1 solution): "+distance);
+    }
+
+    private static void findFirstValueAboveGoalNumber() {
+        if (goal < 1) throw new IllegalArgumentException("Illegal goal: "+goal);
+        Integer a = null, b = null, c = null, // -1,1;  0,1;  1,1;   +^+  <---x+++>
+                d = null, /* n=1 */ e = null, // -1,0;  0,0;  1,0; y: |
+                f = null, g = null, h = null; // -1,-1; 0,-1; 1,-1;  -v-
+        int n = 1, xM1 = -1, x = 0, xP1 = 1, yM1 = -1, y = 0, yP1 = 1,
+                stepsPerDirection = 0, stepsTakenInDirection = 0;
+        CardinalDirection direction = SOUTH;
+        HashMap<String, Integer> coordsToVals = new HashMap<>();
+        while (n <= goal) {
+            coordsToVals.put(x+","+y, n);
+            if (stepsTakenInDirection >= stepsPerDirection) {
+                direction = CardinalDirection.rotateLeft(direction);
+                stepsTakenInDirection = 0;
+                switch (direction) { case EAST, WEST -> stepsPerDirection++; }
+            }
+            stepsTakenInDirection++;
+            switch (direction) {
+/*  ------>  */ case EAST -> {
+/*  A  B  C  */     x++; xM1++; xP1++;
+/*  D  n  e  */     a = b; b = c; c = coordsToVals.get(xP1+","+yP1); d = n;
+/*  f  g  h  */     if (a!=null) { n+=a; if (b!=null) { n+=b; if (c!=null) n+=c; } }
+                }
+/* ^ A  b  c */ case NORTH -> {
+/* | D  n  e */     y++; yM1++; yP1++;
+/* | F  G  h */     f = d; d = a; a = coordsToVals.get(xM1+","+yP1); g = n;
+                    if (f!=null) { n+=f; if (d!=null) { n+=d; if (a!=null) n+=a; } }
+                }
+/*  a  b  c  */ case WEST -> {
+/*  d  n  E  */     x--; xM1--; xP1--;
+/*  F  G  H  */     h = g; g = f; f = coordsToVals.get(xM1+","+yM1); e = n;
+/*  <------  */     if (h!=null) { n+=h; if (g!=null) { n+=g; if (f!=null) n+=f; } }
+                }
+/* a  B  C | */ case SOUTH -> {
+/* d  n  E | */     y--; yM1--; yP1--;
+/* f  g  H V */     c = e; e = h; h = coordsToVals.get(xP1+","+yM1); b = n;
+                    if (c!=null) { n+=c; if (e!=null) { n+=e; if (h!=null) n+=h; } }
+                }
+                default -> throw new IllegalArgumentException("Illegal direction: "+direction);
+            }
+        }
+        System.out.println("\nFirst value written greater than "+goal+": "+n);
     }
 
 }
