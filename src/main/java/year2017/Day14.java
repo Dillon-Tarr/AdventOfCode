0d993652d325cadd3996008be13dcfd87a9a4301
Dillon-Tarr <dill.tarr@gmail.com>
2025-11-07 09:41:31 -0500

    Solve year 2017 day 14 part 2

--- src/main/java/year2017/Day14.java
+++ src/main/java/year2017/Day14.java
@@ -12,12 +12,14 @@ class Day14 {
     static private final int DAY = 14;
     static private final File INPUT_FILE = new File("input-files/2017/"+DAY+".txt");
     static private String inputString;
+    static private final boolean[][] needGroupedBits = new boolean[128][];
 
     static void main() {
         long startTime = System.nanoTime();
 
         getInputData();
         countUsedSquares();
+        countSetBitCoordinatesGroups();
 
         System.out.println("\nExecution time in seconds: "+((double) (System.nanoTime()-startTime)/1000000000));
     }
@@ -30,9 +32,13 @@ class Day14 {
 
     private static void countUsedSquares() {
         int usedSquareCount = 0;
-        for (int i = 0; i <= 127; i++) {
+        for (int i = 0; i < 128; i++) {
+            boolean[] row = new boolean[128]; needGroupedBits[i] = row;
             String binaryString = convertHexStringToBinaryString(getKnotHash(inputString+'-'+i));
-            for (int j = 0; j < binaryString.length(); j++) if (binaryString.charAt(j) == '1') usedSquareCount++;
+            for (int j = 0; j < 128; j++) if (binaryString.charAt(j) == '1') {
+                usedSquareCount++;
+                row[j] = true;
+            }
         }
         System.out.println("\nUsed square count (part 1 answer): "+usedSquareCount);
     }
@@ -82,6 +88,29 @@ class Day14 {
         rangeLengths[inputLength+3] = 47;
         rangeLengths[inputLength+4] = 23;
         return rangeLengths;
+    }
+
+    private static void countSetBitCoordinatesGroups() {
+        int usedSquareRegionCount = 0;
+        for (int y = 0; y < 128; y++) {
+            boolean[] row = needGroupedBits[y];
+            for (int x = 0; x < 128; x++) {
+                if (row[x]) {
+                    usedSquareRegionCount++;
+                    groupContiguousBits(y, x);
+                }
+            }
+        }
+        System.out.println("\nNumber of used-square contiguous regions (part 2 answer): "+ usedSquareRegionCount);
+    }
+
+    private static void groupContiguousBits(int y, int x) {
+        needGroupedBits[y][x] = false;
+        int otherY = y-1, otherX = x-1;
+        if (y > 0 && needGroupedBits[otherY][x]) groupContiguousBits(otherY, x);
+        if (y < 127 && needGroupedBits[otherY+=2][x]) groupContiguousBits(otherY, x);
+        if (x > 0 && needGroupedBits[y][otherX]) groupContiguousBits(y, otherX);
+        if (x < 127 && needGroupedBits[y][otherX+=2]) groupContiguousBits(y, otherX);
     }
 
 }
