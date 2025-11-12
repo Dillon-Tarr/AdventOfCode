1f07a78ee0cb143b5195eafbc266d95d1cf3d5b7
Dillon-Tarr <dill.tarr@gmail.com>
2025-11-12 18:55:37 -0500

    Move year 2017 day 23 part 2

---
+++ src/main/java/year2017/day23/Part2.java
@@ -0,0 +1,24 @@
+package year2017.day23;
+
+import shared.math.PrimeFinders;
+
+import java.util.ArrayList;
+
+class Part2 {
+
+    static void main() {
+        long startTime = System.nanoTime();
+
+        runWellWrittenVersionOfTheAssembly();
+
+        System.out.println("\nExecution time in seconds: "+((double) (System.nanoTime()-startTime)/1000000000));
+    }
+
+    private static void runWellWrittenVersionOfTheAssembly() {
+        ArrayList<Integer> primes = PrimeFinders.findPrimesUntilValue(125400);
+        int h = 0;
+        for (int b = 108400; b <= 125400; b+=17) if (!primes.contains(b)) h++;
+        System.out.println("\nh: "+h);
+    }
+
+}
