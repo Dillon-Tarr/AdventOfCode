24da7a00f223eae4faee9b2b19f1dfe9f0075d63
Dillon-Tarr <dill.tarr@gmail.com>
2025-11-17 13:21:56 -0500

    Solve year 2018 day 8 part 1

---
+++ src/main/java/year2018/Day8.java
@@ -0,0 +1,48 @@
+package year2018;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+class Day8 {
+    static private final int DAY = 8;
+    static private final File INPUT_FILE = new File("input-files/2018/"+DAY+".txt");
+    static private int[] numbers;
+
+    static void main() {
+        long startTime = System.nanoTime();
+
+        getAndProcessInputData();
+        solve();
+
+        System.out.println("\nExecution time in seconds: "+((double) (System.nanoTime()-startTime)/1000000000));
+    }
+
+    private static void getAndProcessInputData() {
+        String[] numberStrings;
+        try (BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE))) {
+            numberStrings = br.readLine().split(" ");
+        } catch (IOException e) {throw new RuntimeException(e);}
+        numbers = new int[numberStrings.length];
+        for (int i = 0; i < numbers.length; i++) numbers[i] = Integer.parseInt(numberStrings[i]);
+    }
+
+    private static void solve() {
+        System.out.println("\nMetadata sum (part 1 answer): "+parseNode(0)[0]);
+    }
+
+    private static int[] parseNode(int index) { // return new int[]{metadataSum, index};
+        int childNodeCount = numbers[index++];
+        int metadataEntryCount = numbers[index++];
+        int metadataSum = 0;
+        for (int i = 0; i < childNodeCount; i++) {
+            var childNodeData = parseNode(index);
+            metadataSum += childNodeData[0];
+            index = childNodeData[1];
+        }
+        for (int i = 0; i < metadataEntryCount; i++) metadataSum += numbers[index++];
+        return new int[]{metadataSum, index};
+    }
+
+}
