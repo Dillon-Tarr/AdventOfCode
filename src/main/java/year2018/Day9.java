715c0695e16e27c73c9f121cb773fb66bd099759
Dillon-Tarr <dill.tarr@gmail.com>
2025-11-18 16:22:50 -0500

    Solve year 2018 day 9 part 2

--- src/main/java/year2018/Day9.java
+++ src/main/java/year2018/Day9.java
@@ -4,13 +4,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 import java.util.HashMap;
 
 class Day9 {
     static private final int DAY = 9;
     static private final File INPUT_FILE = new File("input-files/2018/"+DAY+".txt");
-    static private int playerCount, finalScoringMarbleNumber;
+    static private int playerCount, part1FinalScoringMarbleNumber, part2FinalScoringMarbleNumber, scoringMarbleNumber = 0, playerNumber = 0;
+    static private final ArrayDeque<Integer> circle = new ArrayDeque<>();
+    static private final HashMap<Integer, Long> playerToScoreMap = new HashMap<>();
 
     static void main() {
         long startTime = System.nanoTime();
@@ -27,32 +29,34 @@ class Day9 {
             s = br.readLine();
         } catch (IOException e) {throw new RuntimeException(e);}
         playerCount = Integer.parseInt(s.substring(0, s.indexOf(' ')));
-        finalScoringMarbleNumber = Integer.parseInt(s.substring(s.indexOf("h")+2, s.lastIndexOf(' ')))/23*23;
+        int lastMarble = Integer.parseInt(s.substring(s.indexOf("h")+2, s.lastIndexOf(' ')));
+        part1FinalScoringMarbleNumber = lastMarble*23/23;
+        part2FinalScoringMarbleNumber = lastMarble*2300/23;
     }
 
     private static void solve() {
-        ArrayList<Integer> circle = new ArrayList<>(); circle.add(0); circle.add(2); circle.add(1);
-        int marbleNumber = 2, playerNumber = 2, twentyThreeCounter = 2, currentIndex = 1;
-        HashMap<Integer, Integer> playerToScoreMap = new HashMap<>();
-        while (++marbleNumber <= finalScoringMarbleNumber) {
-            if (++playerNumber > playerCount) playerNumber = 1;
-            if (++twentyThreeCounter == 23) {
-                twentyThreeCounter = 0;
-                currentIndex -= 7;
-                if (currentIndex < 0) currentIndex += circle.size();
-                playerToScoreMap.put(playerNumber, playerToScoreMap.getOrDefault(playerNumber, 0)+marbleNumber+circle.remove(currentIndex));
-            } else {
-                currentIndex +=2;
-                if (currentIndex > circle.size()) currentIndex -= circle.size();
-                circle.add(currentIndex, marbleNumber);
-            }
+        circle.add(0);
+        runGameUntil(part1FinalScoringMarbleNumber);
+        System.out.println("\nHighest score (part 1 answer): "+ getCurrentHighestScore());
+        runGameUntil(part2FinalScoringMarbleNumber);
+        System.out.println("\nHighest score (part 2 answer): "+ getCurrentHighestScore());
+    }
+
+    private static void runGameUntil(int finalScoringMarbleNumber) {
+        while ((scoringMarbleNumber+=23) <= finalScoringMarbleNumber) {
+            for (int i = scoringMarbleNumber-22; i < scoringMarbleNumber; i++) { circle.add(circle.remove()); circle.add(i); }
+            playerNumber+=23; while (playerNumber > playerCount) playerNumber -= playerCount;
+            for (int i = 0; i < 7; i++) circle.addFirst(circle.removeLast());
+            playerToScoreMap.put(playerNumber, playerToScoreMap.getOrDefault(playerNumber, 0L)+scoringMarbleNumber+circle.removeLast());
+            circle.add(circle.remove());
         }
-        int highestScore = -1;
-        for (var entry : playerToScoreMap.entrySet()) {
-            int score = entry.getValue();
-            if (score > highestScore) highestScore = score;
-        }
-        System.out.println("\nHighest score (part 1 answer): "+highestScore);
+        scoringMarbleNumber-=23;
+    }
+
+    private static long getCurrentHighestScore() {
+        long highestScore = -1, score;
+        for (var entry : playerToScoreMap.entrySet()) { score = entry.getValue(); if (score > highestScore) highestScore = score; }
+        return highestScore;
     }
 
 }
