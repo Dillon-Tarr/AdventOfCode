7e69d4d000bfdf02fd28148fa9445b61b1f622a8
Dillon-Tarr <dill.tarr@gmail.com>
2026-01-02 14:59:52 -0500

    Solve year 2025 day 10 part 2

--- src/main/java/year2025/Day10.java
+++ src/main/java/year2025/Day10.java
@@ -12,7 +12,6 @@ class Day10 {
     static private final int DAY = 10;
     static private final File INPUT_FILE = new File("input-files/2025/"+DAY+".txt");
     static private final ArrayList<Machine> machines = new ArrayList<>();
-//    static private int activeSearchCount;
 
     static void main() {
         long startTime = System.nanoTime();
@@ -36,7 +35,7 @@ class Day10 {
         for (var s : inputStrings) {
             var split = s.split(" ");
             boolean[] goalState = null;
-            int[] goalCounts = null;
+            int[] startCounts = null;
             var buttons = new ArrayList<int[]>();
             for (int i = 0; i < split.length; i++) {
                 String sub = split[i];
@@ -56,13 +55,13 @@ class Day10 {
                     case '{' -> {
                         sub = sub.substring(1, sub.length()-1);
                         String[] subSplit = sub.split(",");
-                        goalCounts = new int[subSplit.length];
-                        for (int j = 0; j < subSplit.length; j++) goalCounts[j] = Integer.parseInt(subSplit[j]);
+                        startCounts = new int[subSplit.length];
+                        for (int j = 0; j < subSplit.length; j++) startCounts[j] = Integer.parseInt(subSplit[j]);
                     }
                 }
             }
-            if (goalState == null || buttons.isEmpty() || goalCounts == null) throw new RuntimeException("Bad input file.");
-            machines.add(new Machine(goalState, buttons, goalCounts));
+            if (goalState == null || buttons.isEmpty() || startCounts == null) throw new RuntimeException("Bad input file.");
+            machines.add(new Machine(goalState, buttons, startCounts));
         }
     }
 
@@ -73,192 +72,116 @@ class Day10 {
     }
 
     private static int getFewestPressesToAchieveGoalLights(Machine machine) {
-        var goalLights = machine.part1GoalLights; int width = goalLights.length;
-        var buttons = machine.buttons; int buttonCount = buttons.size();
-        int fewestPresses = Integer.MAX_VALUE;
-        for (int binaryCombo = 1; binaryCombo < 1<<buttonCount; binaryCombo++) {
-            boolean[] state = new boolean[width];
+        int buttonCount = machine.buttons.size(), fewestPresses = Integer.MAX_VALUE;
+        var binaryComboInts = machine.oddParityStringToButtonComboIntsMap.get(toBinaryString(machine.part1GoalLights));
+        for (int binaryCombo : binaryComboInts) {
             int presses = 0;
-            for (int i = 0; i < buttonCount; i++) {
-                if ((binaryCombo & (1 << i)) != 0) {
-                    presses++;
-                    var button = buttons.get(i);
-                    for (int j = 0; j < button.length; j++) state[button[j]] = !state[button[j]];
-                }
-                if (presses < fewestPresses && Arrays.equals(state, goalLights)) fewestPresses = presses;
-            }
+            for (int i = 0; i < buttonCount; i++) if ((binaryCombo & (1 << i)) != 0) presses++;
+            if (presses < fewestPresses) fewestPresses = presses;
         }
         return fewestPresses;
     }
 
     private static void solvePart2() {
-        int sum = 0, machineNumber = 0;
-        for (var machine : machines) {
-            System.out.println("\tTrying machine #"+(++machineNumber)+"...");
-//            activeSearchCount = 0;
-            int timesToTryEachButtonInitially = 0;
-            int presses = getFewestRemainingPresses(machine);
-            System.out.println("\tFewest presses: "+presses);
-            sum += presses;
-        }
+        int sum = 0;
+        for (var machine : machines) sum += getFewestNeededPressesForCount(machine);
         System.out.println("\nPart 2 presses sum: "+sum);
-    } // too high: 20088 - without trying repeating biggest buttons first
+    }
 
-    private static int getFewestRemainingPresses(Machine machine) {
-        int fewestPresses = getFewestRemainingPresses(machine, machine.goalCounts);
-        var buttons = machine.buttons; var goalCounts = machine.goalCounts;
-        int buttonCount = buttons.size(), width = goalCounts.length;
-        int timesToTryEachButton = 1;
-        mainLoop: while (fewestPresses == Integer.MAX_VALUE) {
-            for (var button : buttons) {
-                var counts = Arrays.copyOf(goalCounts, width);
-                for (int n : button) if ((counts[n] -= timesToTryEachButton) < 0) continue mainLoop;
+    private static int getFewestNeededPressesForCount(Machine machine) {
+        var buttons = machine.buttons; var startCounts = machine.startCounts;
+        int width = startCounts.length, initialPresses = 1,
+                fewestPresses = getFewestRemainingPresses(machine, machine.startCounts);
+        while (initialPresses < 5 || fewestPresses == Integer.MAX_VALUE) {
+            // initialPresses could be required to be higher for correct fewest presses, depending on puzzle input.
+            // In my case, only 2 are required. I was off by a very small number when not accounting for this.
+            buttonLoop: for (var button : buttons) {
+                var counts = Arrays.copyOf(startCounts, width);
+                for (int n : button) if ((counts[n] -= initialPresses) < 0) continue buttonLoop;
                 int presses = getFewestRemainingPresses(machine, counts);
+                if (presses == Integer.MAX_VALUE) continue;
+                presses += initialPresses;
+                if (presses < fewestPresses) fewestPresses = presses;
+            }
+            initialPresses++;
+        }
+        return fewestPresses;
+    }
+
+    private static int getFewestRemainingPresses(Machine machine, int[] startCounts) {
+        if (intArrayContainsOnlyZeros(startCounts)) return 0;
+        var buttons = machine.buttons;
+        int buttonCount = buttons.size(), width = startCounts.length, fewestPresses = Integer.MAX_VALUE;
+        var goalLights = new boolean[width]; boolean allEven = true; int lightIndex = -1;
+        while (++lightIndex < width)
+            if (startCounts[lightIndex] % 2 == 1) { goalLights[lightIndex] = true; allEven = false; break; }
+        while (++lightIndex < width)
+            if (startCounts[lightIndex] % 2 == 1) goalLights[lightIndex] = true;
+        if (allEven) {
+            int doubleFactor = 1;
+            var counts = Arrays.copyOf(startCounts, width);
+            halvingLoop: while (true) {
+                for (var count : counts) if (count % 2 == 1) break halvingLoop;
+                for (int i = 0; i < width; i++) counts[i] >>= 1;
+                doubleFactor <<= 1;
+            }
+            int subRemaining = getFewestRemainingPresses(machine, counts);
+            fewestPresses = subRemaining == Integer.MAX_VALUE ? Integer.MAX_VALUE : doubleFactor*subRemaining;
+        } else { // Continue with each possible way to make parity even.
+            var buttonComboInts = machine.oddParityStringToButtonComboIntsMap.get(toBinaryString(goalLights));
+            if (buttonComboInts != null) buttonComboLoop: for (int buttonComboInt : buttonComboInts) {
+                int presses = 0;
+                var counts = Arrays.copyOf(startCounts, width);
+                for (int i = 0; i < buttonCount; i++) if ((buttonComboInt & (1 << i)) != 0) {
+                    presses++;
+                    var button = buttons.get(i);
+                    for (int n : button) if (--counts[n] < 0) continue buttonComboLoop;
+                }
+                int doubleFactor = 1;
+                if (!intArrayContainsOnlyZeros(counts)) {
+                    halvingLoop: while (true) {
+                        for (var count : counts) if (count % 2 == 1) break halvingLoop;
+                        for (int i = 0; i < width; i++) counts[i] >>= 1;
+                        doubleFactor <<= 1;
+                    }
+                    int subRemaining = getFewestRemainingPresses(machine, counts);
+                    if (subRemaining == Integer.MAX_VALUE) continue;
+                    presses += doubleFactor*subRemaining;
+                }
                 if (presses < fewestPresses) fewestPresses = presses;
             }
         }
         return fewestPresses;
     }
 
-    private static int getFewestRemainingPresses(Machine machine, int[] goalCounts) {
-        var buttons = machine.buttons;
-        int machineNumber = machine.number;
-//        System.out.println("Active search count increased to: "+(++activeSearchCount));
-        if (machineNumber == 21)
-            return getRemainingPressesWithStack(buttons, goalCounts);
-        boolean anyZeros = false, anyNonZeros = false;
-        for (int count : goalCounts) {
-            if (count < 0) throw new RuntimeException("How less than 0?");
-            if (count == 0) anyZeros = true;
-            else anyNonZeros = true;
-        }
-        if (!anyNonZeros) {
-//            System.out.println("Active search count decreased to: "+(--activeSearchCount));
-            return 0;
-        }
-        int buttonCount = buttons.size(), width = goalCounts.length;
-        int fewestPresses = Integer.MAX_VALUE;
-//        var legalButtons = new ArrayList<>(buttons);
-//        if (anyZeros) {
-//            var illegalCountIndices = new boolean[width];
-//            for (int i = 0; i < width; i++) if (goalCounts[i] == 0) illegalCountIndices[i] = true;
-//            for (var button : buttons) for (int n = 0; n < button.length; n++)
-//                if (illegalCountIndices[button[n]]) { legalButtons.remove(button); break; }
-////            fewestPresses = getRemainingPressesWithStack(legalButtons, goalCounts);
-////            System.out.println("Amount returned from stack: "+fewestPresses);
-//        }
-//        if (machineNumber == 143) {
-//            int highestCount = 500;
-//            for (var count : goalCounts) if (count < highestCount) highestCount = count;
-//            if (highestCount < 12)
-//                return getRemainingPressesWithStack(legalButtons, goalCounts);
-//        }
-        // zeros dealt with.
-        var goalLights = new boolean[width];
-        boolean anyOdds = false;
-        for (int i = 0; i < width; i++) if (goalCounts[i] % 2 == 1) { goalLights[i] = true; anyOdds = true; }
-        if (anyOdds) {
-            ArrayList<Integer> buttonComboInts = machines.get(machineNumber-1).oddnessToButtonComboIntsMap.get(toBinaryString(goalLights));
-            if (buttonComboInts != null) buttonComboLoop: for (int buttonComboInt : buttonComboInts) { // MAYBE DO THIS FOREVER INSTEAD OF STACK??
-                int initialPresses = 0;
-                var counts = Arrays.copyOf(goalCounts, width);
-                for (int i = 0; i < buttonCount; i++) {
-                    if ((buttonComboInt & (1 << i)) != 0) {
-                        initialPresses++;
-                        var button = buttons.get(i);
-                        for (int j = 0; j < button.length; j++) if (--counts[button[j]] < 0) continue buttonComboLoop;
-                    }
-                }
-                anyZeros = false;
-                for (var count : counts) if (count == 0) { anyZeros = true; break; }
-                int doubleFactor = 1;
-                if (!anyZeros) {
-                    halvingLoop: while (true) {
-                        for (var count : counts) if (count % 2 == 1) break halvingLoop;
-                        for (int i = 0; i < width; i++) counts[i] /= 2;
-                        doubleFactor *= 2;
-                    }
-//                    if (machineNumber == 143) {
-//                        for (int i = 0; i < width; i++) counts[i] *= 2;
-//                        doubleFactor /= 2;
-//                    }
-                }
-                int subRemaining = getFewestRemainingPresses(machine, counts);
-                if (subRemaining == Integer.MAX_VALUE) continue;
-                int totalPresses = initialPresses+(doubleFactor*subRemaining);
-                if (totalPresses < fewestPresses) fewestPresses = totalPresses;
-            }
-        } else { // no odds to work out, only evens (no zeros):
-            int doubleFactor = 1;
-            var counts = Arrays.copyOf(goalCounts, width);
-            halvingLoop: while (true) {
-                for (var count : counts) if (count % 2 == 1) break halvingLoop;
-                for (int i = 0; i < width; i++) counts[i] /= 2;
-                doubleFactor *= 2;
-            }
-            int subRemaining = getFewestRemainingPresses(machine, counts);
-            fewestPresses = subRemaining == Integer.MAX_VALUE ? Integer.MAX_VALUE : doubleFactor*subRemaining;
-        }
-//        System.out.println("Active search count decreased to: "+(--activeSearchCount));
-        return fewestPresses;
-    }
-
-    private static int getRemainingPressesWithStack(ArrayList<int[]> buttons, int[] goalCounts) {
-        int width = goalCounts.length;
-        var queue = new PriorityQueue<PressRecord>(); var visited = new HashSet<String>();
-        for (var button : buttons) queue.add(new PressRecord(Arrays.copyOf(goalCounts, width), 0, button, button.length));
-        queueLoop: while (!queue.isEmpty()) {
-            var queuedPress = queue.remove();
-            int[] counts = queuedPress.counts;
-            int presses = 1+queuedPress.presses;
-            for (int n : queuedPress.nextButton) if (--counts[n] < 0) continue queueLoop;
-            boolean onlyZeros = true;
-            for (var count : counts) if (count != 0) { onlyZeros = false; break;}
-            if (onlyZeros) return presses;
-            var sb = new StringBuilder();
-            for (var count : counts) sb.append(count).append(',');
-            if (!visited.add(sb.toString())) continue;
-            buttonLoop: for (int i = 0; i < buttons.size(); i++) {
-                var button = buttons.get(i);
-                for (var n : button) if (counts[n] == 0) continue buttonLoop;
-                queue.add(new PressRecord(Arrays.copyOf(counts, width), presses, button, button.length));
-            }
-        }
-        return Integer.MAX_VALUE;
-    }
-
-    private record PressRecord(int[] counts, int presses, int[] nextButton, int nextButtonLength) implements Comparable<PressRecord> {
-        @Override
-        public int compareTo(PressRecord o) { return Integer.compare(o.nextButtonLength, nextButtonLength); }
-
+    private static boolean intArrayContainsOnlyZeros(int[] ints) {
+        for (int n : ints) if (n != 0) return false;
+        return true;
     }
 
     private static class Machine {
-        static int numberOfMachines = 0;
-
-        int number = ++numberOfMachines;
         boolean[] part1GoalLights;
         ArrayList<int[]> buttons;
-        int[] goalCounts;
-        HashMap<String, ArrayList<Integer>> oddnessToButtonComboIntsMap = new HashMap<>();
+        int[] startCounts;
+        HashMap<String, ArrayList<Integer>> oddParityStringToButtonComboIntsMap = new HashMap<>();
 
-        Machine(boolean[] goalLights, ArrayList<int[]> buttons, int[] goalCounts) {
+        Machine(boolean[] goalLights, ArrayList<int[]> buttons, int[] startCounts) {
             this.part1GoalLights = goalLights;
             this.buttons = buttons;
-            this.goalCounts = goalCounts;
+            this.startCounts = startCounts;
 
-            int width = goalCounts.length, buttonCount = buttons.size();
+            int width = startCounts.length, buttonCount = buttons.size();
             for (int binaryCombo = 1; binaryCombo < 1<<buttonCount; binaryCombo++) {
                 boolean[] state = new boolean[width];
                 for (int i = 0; i < buttonCount; i++) {
                     if ((binaryCombo & (1 << i)) != 0) {
                         var button = buttons.get(i);
-                        for (int j = 0; j < button.length; j++) state[button[j]] = !state[button[j]];
+                        for (int n : button) state[n] = !state[n];
                     }
                 }
                 var stateString = toBinaryString(state);
-                var ints = oddnessToButtonComboIntsMap.getOrDefault(stateString, new ArrayList<>());
-                if (ints.isEmpty()) oddnessToButtonComboIntsMap.put(stateString, ints);
+                var ints = oddParityStringToButtonComboIntsMap.getOrDefault(stateString, new ArrayList<>());
+                if (ints.isEmpty()) oddParityStringToButtonComboIntsMap.put(stateString, ints);
                 ints.add(binaryCombo);
             }
         }
