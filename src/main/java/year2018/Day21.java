062f43ab85fc6603d5bae154db73b3d4ab4276d5
Dillon-Tarr <dill.tarr@gmail.com>
2026-01-05 16:28:59 -0500

    Solve year 2018 day 21

---
+++ src/main/java/year2018/Day21.java
@@ -0,0 +1,98 @@
+package year2018;
+
+import shared.BitwiseOperations;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+class Day21 {
+    static private final int DAY = 21;
+    static private final File INPUT_FILE = new File("input-files/2018/"+DAY+".txt");
+    static private final long[] registers = new long[6];
+    static private int instructionPointerNumber, instructionCount;
+    static private final ArrayList<int[]> instructions = new ArrayList<>();
+    static private final ArrayList<String> instructionNames = new ArrayList<>(List.of(new String[]{"addr", "addi", "mulr",
+            "muli", "banr", "bani", "borr", "bori", "setr", "seti", "gtir", "gtri", "gtrr", "eqir", "eqri", "eqrr"}));
+
+    static void main() {
+        long startTime = System.nanoTime();
+
+        getAndProcessInputData();
+        solve();
+
+        System.out.println("\nExecution time in seconds: "+((double) (System.nanoTime()-startTime)/1000000000));
+    }
+
+    private static void getAndProcessInputData() {
+        ArrayList<String> instructionStrings = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE))) {
+            String s = br.readLine();
+            instructionPointerNumber = Character.getNumericValue(s.charAt(s.length()-1));
+            s = br.readLine();
+            while (s != null) {
+                instructionStrings.add(s);
+                s = br.readLine();
+            }
+        } catch (IOException e) {throw new RuntimeException(e);}
+        for (String s : instructionStrings) {
+            String[] parts = s.split(" ");
+            int[] instruction = new int[4];
+            instruction[0] = -1;
+            for (int i = 0; i < instructionNames.size(); i++) if (parts[0].equals(instructionNames.get(i))) { instruction[0] = i; break; }
+            if (instruction[0] == -1) throw new RuntimeException("Invalid instruction name: "+parts[0]);
+            instruction[1] = Integer.parseInt(parts[1]);
+            instruction[2] = Integer.parseInt(parts[2]);
+            instruction[3] = Integer.parseInt(parts[3]);
+            instructions.add(instruction);
+        }
+        instructionCount = instructions.size();
+    }
+
+    private static void solve() {
+        long instructionNumber; int[] instruction;
+        var seen = new HashSet<Long>();
+        long part1Answer = -1, lastSeen = -1;
+        while (true) {
+            instructionNumber = registers[instructionPointerNumber];
+            instruction = instructions.get((int)registers[instructionPointerNumber]);
+            if (instructionNumber == 28) {
+                long value = registers[1];
+                if (part1Answer == -1) part1Answer = value;
+                if (seen.add(value)) lastSeen = value;
+                else break;
+            }
+            executeInstruction(instruction);
+        }
+        System.out.println("\nLowest value with which to initialize register 0 and halt the program (part 1 answer): "+part1Answer);
+        System.out.println("\nLowest value with which to initialize r0 while maximizing instructions executed (part 2 answer): "+lastSeen);
+    }
+
+    private static void executeInstruction(int[] instruction) {
+        switch (instruction[0]) {
+            case 0 -> registers[instruction[3]] = registers[instruction[1]]+registers[instruction[2]]; // addr
+            case 1 -> registers[instruction[3]] = registers[instruction[1]]+instruction[2]; // addi
+            case 2 -> registers[instruction[3]] = registers[instruction[1]]*registers[instruction[2]]; // mulr
+            case 3 -> registers[instruction[3]] = registers[instruction[1]]*instruction[2]; // muli
+            case 4 -> registers[instruction[3]] = BitwiseOperations.and((int)registers[instruction[1]], (int)registers[instruction[2]]); // banr
+            case 5 -> registers[instruction[3]] = BitwiseOperations.and((int)registers[instruction[1]], (int)instruction[2]); //bani
+            case 6 -> registers[instruction[3]] = BitwiseOperations.or((int)registers[instruction[1]], (int)registers[instruction[2]]); // borr
+            case 7 -> registers[instruction[3]] = BitwiseOperations.or((int)registers[instruction[1]], (int)instruction[2]); // bori
+            case 8 -> registers[instruction[3]] = registers[instruction[1]]; // setr
+            case 9 -> registers[instruction[3]] = instruction[1]; // seti
+            case 10 -> registers[instruction[3]] = instruction[1] > registers[instruction[2]] ? 1 : 0; // gtir
+            case 11 -> registers[instruction[3]] = registers[instruction[1]] > instruction[2] ? 1 : 0; // gtri
+            case 12 -> registers[instruction[3]] = registers[instruction[1]] > registers[instruction[2]] ? 1 : 0; // gtrr
+            case 13 -> registers[instruction[3]] = instruction[1] == registers[instruction[2]] ? 1 : 0; // eqir
+            case 14 -> registers[instruction[3]] = registers[instruction[1]] == instruction[2] ? 1 : 0; // eqri
+            case 15 -> registers[instruction[3]] = registers[instruction[1]] == registers[instruction[2]] ? 1 : 0; // eqrr
+            default -> throw new IllegalArgumentException("Illegal instruction type number: "+instruction[0]);
+        }
+        registers[instructionPointerNumber]++;
+    }
+
+}
