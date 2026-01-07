49cc4bcf49f1fa1eb13556292b43de6e56c284c0
Dillon-Tarr <dill.tarr@gmail.com>
2026-01-07 18:59:34 -0500

    Solve year 2018 day 22 part 2

--- src/main/java/year2018/Day22.java
+++ src/main/java/year2018/Day22.java
@@ -4,12 +4,20 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 class Day22 {
     static private final int DAY = 22;
     static private final File INPUT_FILE = new File("input-files/2018/"+DAY+".txt");
-    static private int DEPTH, TARGET_Y, TARGET_X, Y_BOUNDARY, X_BOUNDARY;
-    static private int[][] regionTypes;
+    static private int DEPTH, TARGET_Y, TARGET_X;
+    static private int[][] regions;
+    static private int[][][] yXEquipMinRecords;
+    static private final Integer[][] equipSwapArray
+            = new Integer[][] { { null, 2, 1 }, { 2, null, 0 }, { 1, 0, null } };
+    // region types: 0, 1, 2 (rocky, wet, narrow); equipment types: 0, 1, 2 (none, torch, climbingGear);
+    // allowed equipment types by region type (region > equipment):
+    // 0 > 1, 2; 1 > 0, 2; 2 > 0, 1
 
     static void main() {
         long startTime = System.nanoTime();
@@ -31,61 +39,90 @@ class Day22 {
             TARGET_Y = Integer.parseInt(ss[1]);
             TARGET_X = Integer.parseInt(ss[0]);
         } catch (IOException e) {throw new RuntimeException(e);}
-        Y_BOUNDARY = TARGET_Y + 999;
-        X_BOUNDARY = TARGET_X + 999;
     }
 
     private static void prepare() {
-        var erosionLevels = new int[Y_BOUNDARY + 1][X_BOUNDARY + 1];
-        regionTypes = new int[Y_BOUNDARY+1][X_BOUNDARY+1];
-        erosionLevels[0][0] = calculateErosionLevel(0);
-        for (int x = 1; x <= X_BOUNDARY; x++) {
+        int FAR_BOUNDARY = Math.max(TARGET_Y, TARGET_X)*6/5; // == the greater target dimension + 20%
+        var erosionLevels = new int[FAR_BOUNDARY + 1][FAR_BOUNDARY + 1];
+        regions = new int[FAR_BOUNDARY + 1][FAR_BOUNDARY + 1];
+        erosionLevels[0][0] = erosionLevels[TARGET_Y][TARGET_X] = calculateErosionLevel(0);
+        for (int x = 1; x <= FAR_BOUNDARY; x++) {
             int level = calculateErosionLevel(x * 16807);
-            erosionLevels[0][x] = level; regionTypes[0][x] = level % 3;
+            erosionLevels[0][x] = level; regions[0][x] = level % 3;
         }
-        for (int y = 1; y <= Y_BOUNDARY; y++) {
+        for (int y = 1; y <= FAR_BOUNDARY; y++) {
             int level = calculateErosionLevel(y * 48271);
-            erosionLevels[y][0] = level; regionTypes[y][0] = level % 3;
+            erosionLevels[y][0] = level; regions[y][0] = level % 3;
         }
-        for (int y = 1; y <= Y_BOUNDARY; y++) {
+        for (int y = 1; y <= FAR_BOUNDARY; y++) {
             int leftLevel = erosionLevels[y][0];
             if (y == TARGET_Y) {
                 for (int x = 1; x < TARGET_X; x++) {
                     leftLevel = calculateErosionLevel(leftLevel * erosionLevels[y-1][x]);
-                    erosionLevels[y][x] = leftLevel; regionTypes[y][x] = leftLevel % 3;
+                    erosionLevels[y][x] = leftLevel; regions[y][x] = leftLevel % 3;
                 }
                 leftLevel = erosionLevels[0][0];
-                for (int x = TARGET_X+1; x <= X_BOUNDARY; x++) {
+                for (int x = TARGET_X+1; x <= FAR_BOUNDARY; x++) {
                     leftLevel = calculateErosionLevel(leftLevel * erosionLevels[y-1][x]);
-                    erosionLevels[y][x] = leftLevel; regionTypes[y][x] = leftLevel % 3;
+                    erosionLevels[y][x] = leftLevel; regions[y][x] = leftLevel % 3;
                 }
             } else {
-                for (int x = 1; x <= X_BOUNDARY; x++) {
+                for (int x = 1; x <= FAR_BOUNDARY; x++) {
                     leftLevel = calculateErosionLevel(leftLevel * erosionLevels[y-1][x]);
-                    erosionLevels[y][x] = leftLevel; regionTypes[y][x] = leftLevel % 3;
+                    erosionLevels[y][x] = leftLevel; regions[y][x] = leftLevel % 3;
                 }
+            }
+        }
+        yXEquipMinRecords = new int[FAR_BOUNDARY +1][FAR_BOUNDARY+1][3];
+        for (int y = 0; y <= FAR_BOUNDARY; y++) {
+            for (int x = 0; x <= FAR_BOUNDARY; x++) {
+                var records = yXEquipMinRecords[y][x];
+                records[0] = 99999; records[1] = 99999; records[2] = 99999;
             }
         }
     }
 
-    private static int calculateErosionLevel(int geologicIndex) {
-        return (geologicIndex + DEPTH) % 20183;
-    }
+    private static int calculateErosionLevel(int geologicIndex) { return (geologicIndex + DEPTH) % 20183; }
 
     private static void solvePart1() {
-        int riskLevelSum = 0;
-        for (int y = 0; y <= TARGET_Y; y++) for (int x = 0; x <= TARGET_X; x++) riskLevelSum += regionTypes[y][x];
-        System.out.println("\nRisk level sum (part 1 answer): "+riskLevelSum);
+        int sum = 0;
+        for (int y = 0; y <= TARGET_Y; y++) for (int x = 0; x <= TARGET_X; x++) sum += regions[y][x];
+        System.out.println("\nRisk level sum (part 1 answer): "+ sum);
     }
 
     private static void solvePart2() {
-//        region types: 0, 1, 2 (rocky, wet, narrow)
-//        equipment types: 0, 1, 2 (none, torch, climbingGear)
-//        allowed equipment types by region type (region > equipment):
-//        0 > 1, 2
-//        1 > 0, 2
-//        2 > 0, 1
+        int fewestMins = 99999;
+        var queue = new PriorityQueue<TravelRecord>(Comparator.comparingInt(r -> r.minutes));
+        queue.add(new TravelRecord(0, 0, 0, 1, 0));
+        while (!queue.isEmpty()) {
+            var travelRecord = queue.poll();
+            int mins = travelRecord.minutes;
+            if (mins >= fewestMins) break;
+            int y = travelRecord.y, x = travelRecord.x, equip = travelRecord.equipmentType;
+            if (y == TARGET_Y && x == TARGET_X) {
+                if (equip != 1) mins += 7;
+                if (mins < fewestMins) fewestMins = mins;
+                continue;
+            }
+            int swEquip = equipSwapArray[travelRecord.regionType][equip], swMins = mins + 7;
+            var hereRecords = yXEquipMinRecords[y][x];
+            if (mins >= hereRecords[equip]) continue;
+            hereRecords[equip] = mins;
+            if (swMins < hereRecords[swEquip]) hereRecords[swEquip] = swMins;
+            queue.add(newRecord(++y, x, equip, ++mins, swEquip, ++swMins)); // south
+            if ((y -= 2) >= 0) queue.add(newRecord(y, x, equip, mins, swEquip, swMins)); // north
+            queue.add(newRecord(++y, ++x, equip, mins, swEquip, swMins)); // east
+            if ((x -= 2) > 0) queue.add(newRecord(y, x, equip, mins, swEquip, swMins)); // west
+        }
+        System.out.println("\nFewest minutes needed to rescue friend (part 2 answer): "+ fewestMins);
+    }
 
+    private record TravelRecord(int y, int x, int regionType, int equipmentType, int minutes){}
+
+    private static TravelRecord newRecord(int y, int x, int equip, int mins, int swEquip, int swMins) {
+        int region = regions[y][x];
+        return region == equip ? new TravelRecord(y, x, region, swEquip, swMins)
+                : new TravelRecord(y, x, region, equip, mins);
     }
 
 }
