4784ea40a0028866e32076fc171ed73fc3dcca60
Dillon-Tarr <dill.tarr@gmail.com>
2026-01-12 11:42:26 -0500

    Partially solve year 2018 day 23 part 2

--- src/main/java/year2018/Day23.java
+++ src/main/java/year2018/Day23.java
@@ -10,6 +10,8 @@ class Day23 {
     static private final int DAY = 23;
     static private final File INPUT_FILE = new File("input-files/2018/"+DAY+".txt");
     static private final ArrayList<Nanobot> nanobots = new ArrayList<>();
+    static private final int[] stepsPerDirectionByPowerOfTwoStepSize = new int[] // [26] 2pow(0-25)
+            { 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 3 };
 
     static void main() {
         long startTime = System.nanoTime();
@@ -52,8 +54,52 @@ class Day23 {
         System.out.println("\nNumber of nanobots in range of the strongest nanobot (part 1 answer): "+count);
     }
 
+    /**
+     * Part 2 solution inspired by:
+     * <a href="https://www.reddit.com/r/adventofcode/comments/a8s17l/comment/echapvu/">wzkx on Reddit</a>
+     */
     private static void solvePart2() {
-        System.out.println("\n... (part 2 answer): ?");
+        long xSum = 0, ySum = 0, zSum = 0;
+        for (var bot : nanobots) { xSum += bot.x; ySum += bot.y; zSum += bot.z; }
+        int botCount = nanobots.size();
+        int xMean = (int)(xSum/botCount), yMean = (int)(ySum/botCount),  zMean = (int)(zSum/botCount);
+        System.out.println("""
+                
+                \t\t\t\t\tResults  of  part  2  search:\s
+                -----------------------------------------------------------------------------------------
+                   Step  | No. of steps  |       Best coordinates        | Best   x how many points found
+                   size  | per direction |     x:        y:        z:    | count  x   with that count""");
+        int highCountCount;
+        for (int stepSizePowerOfTwo = 25; stepSizePowerOfTwo >= 0; stepSizePowerOfTwo--) {
+            highCountCount = 0;
+            int stepSize = 1 << stepSizePowerOfTwo;
+            int perDirSteps = stepsPerDirectionByPowerOfTwoStepSize[stepSizePowerOfTwo];
+            int highCount = 0, xOfHighCount = 0, yOfHighCount = 0, zOfHighCount = 0;
+            for (int xShift = -perDirSteps; xShift <= perDirSteps; xShift++) {
+                int x = xMean + (stepSize * xShift);
+                for (int yShift = -perDirSteps; yShift <= perDirSteps; yShift++) {
+                    int y = yMean + (stepSize * yShift);
+                    for (int zShift = -perDirSteps; zShift <= perDirSteps; zShift++) {
+                        int z = zMean + (stepSize * zShift);
+                        int count = 0;
+                        for (var b : nanobots) if (Math.abs(b.x - x) + Math.abs(b.y - y) + Math.abs(b.z - z) <= b.range) count++;
+                        if (count > highCount) {
+                            highCount = count;
+                            xOfHighCount = x;
+                            yOfHighCount = y;
+                            zOfHighCount = z;
+                            highCountCount = 1;
+                        } else if (count == highCount) highCountCount++;
+                    }
+                }
+            }
+            System.out.printf("%8s |      %3s      | %9s,%9s,%9s | %3s    x%d\n", stepSize, perDirSteps, xOfHighCount, yOfHighCount, zOfHighCount, highCount, highCountCount);
+            xMean = xOfHighCount;
+            yMean = yOfHighCount;
+            zMean = zOfHighCount;
+        }
+        int x = Math.abs(xMean), y = Math.abs(yMean), z = Math.abs(zMean);
+        System.out.println("\nManhattan distance of the closest-to-0,0,0 point of those closest to the most nodes (part 2 answer): " + (x + y + z));
     }
 
     private record Nanobot (int x, int y, int z, int range) {}
