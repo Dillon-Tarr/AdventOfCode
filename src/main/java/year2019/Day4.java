ce32d7d83af6f9454d97767bb1ebc4bf1d756082
Dillon-Tarr <dill.tarr@gmail.com>
2026-01-30 09:50:49 -0500

    Optimize for speed

--- src/main/java/year2019/Day4.java
+++ src/main/java/year2019/Day4.java
@@ -24,6 +24,13 @@ class Day4 {
     }
 
     private static void solve() {
+        int[][] addAmountsByIndexAndDiff = new int[][]{
+                {},
+                {0, 10_000, 20_000, 30_000, 40_000, 50_000, 60_000, 70_000, 80_000, 90_000},
+                {0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000},
+                {0, 100, 200, 300, 400, 500, 600, 700, 800, 900},
+                {0, 10, 20, 30, 40, 50, 60, 70, 80, 90}, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+        };
         int count1 = 0, count2 = 0;
         int n = rangeStart;
         nLoop: while (n <= rangeEnd) {
@@ -31,7 +38,11 @@ class Day4 {
             var ca = String.valueOf(n).toCharArray(); char c1, c2 = ca[0];
             for (int i = 1; i < 6; i++) {
                 c1 = c2; c2 = ca[i];
-                if (c1 > c2) { n += (c1-c2)*Math.powExact(10, -(i-5)); continue nLoop; }
+                if (c1 > c2) {
+                    n += addAmountsByIndexAndDiff[i][c1-c2];
+                    while ((++i < 6)) { c2 = ca[i]; if (c1 > c2) n += addAmountsByIndexAndDiff[i][c1-c2]; }
+                    continue nLoop;
+                }
                 if (c1 == c2) {
                     if (!foundNonTrioPair && !lastCheckWasPair && (i == 5 || c2 != ca[i+1])) foundNonTrioPair = true;
                     lastCheckWasPair = foundPair = true;
