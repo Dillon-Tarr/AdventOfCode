76d53c16695cb180972b9b132ed2a75be4377c97
Dillon-Tarr <dill.tarr@gmail.com>
2026-02-02 10:31:03 -0500

    Improve handling when on last number; improve variable names

--- src/main/java/shared/math/PermutationGenerators.java
+++ src/main/java/shared/math/PermutationGenerators.java
@@ -4,25 +4,28 @@ import java.util.ArrayList;
 
 public class PermutationGenerators {
 
-    public static ArrayList<int[]> generatePermutations(int numberOfDigits){
-        return getSubPermutations(numberOfDigits, new int[]{});
+    /**
+     * @param numberOfNumbers the number of numbers, counting up from 0, for which to generate all permutations
+     * @return an ArrayList&lt;int[]> of all the permutations of the digits from 0 up to (not including) <code>numberOfNumbers</code>
+     */
+    public static ArrayList<int[]> generatePermutations(int numberOfNumbers){
+        return getSubPermutations(numberOfNumbers, new int[]{});
     }
 
-    private static ArrayList<int[]> getSubPermutations(int neededDigitCount, int[] existingDigits) {
+    private static ArrayList<int[]> getSubPermutations(int neededNumberCount, int[] existingNumbers) {
         ArrayList<int[]> subPermutations = new ArrayList<>();
-        int existingDigitCount = existingDigits.length;
-        int[] workingDigits = new int[existingDigitCount+1];
-        for (int i = 0; i < existingDigitCount; i++) workingDigits[i] = existingDigits[i];
-        boolean digitAlreadyUsed, thisIsTheLastDigit = existingDigitCount == neededDigitCount-1;
-        for (int i = 0; i < neededDigitCount; i++) {
-            digitAlreadyUsed = false;
-            for (int d = 0; d < existingDigitCount; d++)
-                if (existingDigits[d] == i) { digitAlreadyUsed = true; break; }
-            if (!digitAlreadyUsed) {
-                workingDigits[existingDigitCount] = i;
-                if (!thisIsTheLastDigit) subPermutations.addAll(getSubPermutations(neededDigitCount, workingDigits));
-                else subPermutations.add(workingDigits);
-            }
+        int existingNumberCount = existingNumbers.length;
+        int[] workingNumbers = new int[existingNumberCount+1];
+        for (int i = 0; i < existingNumberCount; i++) workingNumbers[i] = existingNumbers[i];
+        if (existingNumberCount == neededNumberCount-1) nLoop: for (int n = 0; n < neededNumberCount; n++) {
+            for (int i = 0; i < existingNumberCount; i++) if (existingNumbers[i] == n) continue nLoop;
+            workingNumbers[existingNumberCount] = n;
+            subPermutations.add(workingNumbers);
+            break;
+        } else nLoop: for (int n = 0; n < neededNumberCount; n++) {
+            for (int i = 0; i < existingNumberCount; i++) if (existingNumbers[i] == n) continue nLoop;
+            workingNumbers[existingNumberCount] = n;
+            subPermutations.addAll(getSubPermutations(neededNumberCount, workingNumbers));
         }
         return subPermutations;
     }
